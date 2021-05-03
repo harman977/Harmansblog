8682f3b7d1926457afa65a847022822018390d00
harman977 <paulh@guilford.edu>
2021-05-03 00:39:14 -0400

    Progress website

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+    "use strict";
+    
+    // Initiate the wowjs
+    new WOW().init();
+    
+    
+    
+    
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $('.nav-bar').addClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "73px");
+        } else {
+            $('.nav-bar').removeClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "0");
+        }
+    });
+    
+    
+    
+    
+    
+    
