3f78296907b6b9e27a9a596dcf25e86fad131838
harman977 <paulh@guilford.edu>
2021-05-03 00:55:44 -0400

    navbar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 (function ($) {
     "use strict";
     
-    // Initiate the wowjs
+    
     new WOW().init();
     
     
     
     
     
-    // Sticky Navbar
+  
     $(window).scroll(function () {
         if ($(this).scrollTop() > 90) {
             $('.nav-bar').addClass('nav-sticky');
@@ -19,6 +19,25 @@
         }
     });
     
+
+
+
+
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
     
     
     
